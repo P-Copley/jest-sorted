f31f409a53a65154280ebcdbd3da88e572eca1ca
P-Copley <pkcopley@gmail.com>
2020-04-08 17:01:06 +0100

    extend expect with toBeSorted

---
+++ index.js
@@ -0,0 +1,17 @@
+const { toBeSorted } = require("./src/sorted");
+
+const jestExpect = global.expect;
+
+console.log("hai");
+if (jestExpect !== undefined) {
+  jestExpect.extend({
+    toBeSorted,
+  });
+} else {
+  /* eslint-disable no-console */
+  console.error(
+    "Unable to find Jest's global expect." +
+      "\nPlease check you have added jest-extended correctly to your jest configuration." +
+      "\nSee https://github.com/jest-community/jest-extended#setup for help."
+  );
+}
