bfc4dfa68761d092d794d5f940d7ff8757498eaa
P-Copley <pkcopley@gmail.com>
2020-04-08 17:01:24 +0100

    smoke tests toBeSorted

---
+++ __tests__/index.spec.js
@@ -0,0 +1,5 @@
+describe("expect.toBeSorted", () => {
+  it("exists", () => {
+    expect(typeof expect.toBeSorted).toBe("function");
+  });
+});
---
+++ src/sorted.js
@@ -0,0 +1,1 @@
+exports.toBeSorted = (recieved, options) => {};
