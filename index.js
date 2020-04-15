fac1581fef32b39d18e9e30d42f6b5b63caa6dec
P-Copley <pkcopley@gmail.com>
2020-04-15 11:18:18 +0100

    blantantly steal jest-extendeds error message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (jestExpect !== undefined) {
   /* eslint-disable no-console */
   console.error(
     "Unable to find Jest's global expect." +
-      "\nPlease check you have added jest-extended correctly to your jest configuration." +
+      "\nPlease check you have added jest-sorted correctly to your jest configuration." +
       "\nSee https://github.com/jest-community/jest-extended#setup for help."
   );
 }
