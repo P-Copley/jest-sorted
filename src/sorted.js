7a08ecf9425271d7982e46557b62b8a344388175
P-Copley <pkcopley@gmail.com>
2020-11-05 10:12:31 +0000

    use > or < comparison for all values in defaultCompare

--- src/sorted.js
+++ src/sorted.js
@@ -1,13 +1,8 @@
 const defaultCompare = (a, b) => {
-  if (a === undefined && b === undefined) return 0;
-
-  if (a === undefined) return 1;
-
-  if (b === undefined) return -1;
-
   if (a > b) {
     return 1;
-  } else if (a < b) {
+  }
+  if (a < b) {
     return -1;
   }
   return 0;
